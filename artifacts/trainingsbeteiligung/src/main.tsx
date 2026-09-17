e87b2763752360912ae66c884f453b319440210c
eschlbacher <eschlbacher@googlemail.com>
2026-09-18 00:33:49 +0200

    Teams-Builder in App einbinden

--- artifacts/trainingsbeteiligung/src/main.tsx
+++ artifacts/trainingsbeteiligung/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import TeamsBuilder from './TeamsBuilder';
 import { ErrorBoundary } from '@/components/error-boundary';
 
 import './index.css';
@@ -13,5 +14,6 @@ createRoot(document.getElementById('root')!, {
 }).render(
   <ErrorBoundary>
     <App />
+    <TeamsBuilder />
   </ErrorBoundary>,
 );
